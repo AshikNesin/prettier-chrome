6712b7041fd5600215e1e3221510468c344551ac
Ashik Nesin <mail@ashiknesin.com>
2017-07-31 16:38:35 +0530

    WIP: Add prettier & select <pre> blocks

---
+++ README.md
@@ -0,0 +1,11 @@
+# Prettier Chrome
+Format JavaScript code block with Prettier
+
+💡 [Browser extension: Format JS code blocks with Prettier #99
+](https://github.com/sindresorhus/module-requests/issues/99)
+
+
+### Reference
+
+https://github.com/jgierer12/prettier-github
+
--- src/content.js
+++ src/content.js
@@ -1,1 +1,16 @@
-console.log('hello from prettier chrome');
+import domLoaded from 'dom-loaded'
+
+domLoaded.then(() => {
+	// Find all the JS Code Blocks
+	const codeBlocks = document.querySelectorAll('pre');
+
+	[...codeBlocks].map((el) => {
+		const code = el.innerText;
+		const formattedCode = prettier.format(code);
+		console.log(formattedCode);
+		// el.innerText = formattedCode;
+	})
+
+});
+
+
